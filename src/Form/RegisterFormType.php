292250a98e57b783d2a9a46b93e74f0560258248
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-01-22 10:13:54 +0100

    Add lastName and firstName fields in User entity and RegisterFormType

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,40 @@ class RegisterFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
-        ;
+            ->add('firstName', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir votre prénom'
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir votre adresse email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir votre mot de passe'
+                ]
+            ])
+            ->add('password_confirm', PasswordType::class, [
+                'label' => 'Confirmez votre mot de passe',
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Confirmez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
