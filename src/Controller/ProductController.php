5ab89ff4a456e069285c4ad7e3e34c7df0dc71a3
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-02-22 22:24:54 +0100

    Add style to index product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/nos-produits", name="product_index")
+     */
+    public function index(ProductRepository $productRepository, EntityManagerInterface $em): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
