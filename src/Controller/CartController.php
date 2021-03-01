4d52b20afab4b7e69dc9bc333fc5262327665019
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-03-01 20:21:40 +0100

    Add data and style to index cart template

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,20 +3,38 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart_index")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->getCart());
+        $cartComplete = [];
 
-        return $this->render('cart/index.html.twig');
+        foreach ($cart->getCart() as $id => $quantity) {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cartComplete
+        ]);
     }
 
     /**
