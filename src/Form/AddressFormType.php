21c357004d9f6a30a99fb56ba83a70fd94c22c2c
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-03-08 10:21:55 +0100

    Add AddressFormType with submittype to replace user field

---
+++ src/Form/AddressFormType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('company')
+            ->add('address')
+            ->add('postal')
+            ->add('city')
+            ->add('country')
+            ->add('phone')
+            ->add('submit', SubmitType::class, [
+                'label' => 'Ajouter mon adresse'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
