79f1df75bd179d08beccb9872b5e6d38cd0f40d5
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-03-08 11:27:03 +0100

    System to ad address is OK with Bootstrap card

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Form\AddressFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountAddressController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+    }
+
     /**
      * @Route("/compte/adresses", name="account_address")
      */
@@ -19,8 +31,25 @@ class AccountAddressController extends AbstractController
     /**
      * @Route("/compte/ajouter-une-adresse", name="add_account_address")
      */
-    public function addAddress(): Response
+    public function addAddress(Request $request): Response
     {
-        return $this->render('account/add_address.html.twig');
+        $address = new Address();
+
+        $form = $this->createForm(AddressFormType::class, $address);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/add_address.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
