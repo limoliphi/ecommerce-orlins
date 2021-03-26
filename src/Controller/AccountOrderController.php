26574d31eec727ba3dfbb014193041076c23a063
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-03-26 11:21:55 +0100

    Displaying order in user area OK

--- src/Controller/AccountOrderController.php
+++ src/Controller/AccountOrderController.php
@@ -2,17 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountOrderController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/compte/mesc-commandes", name="account_order")
+     * @Route("/compte/mes-commandes", name="account_order")
      */
     public function index(): Response
     {
-        return $this->render('account/index_order.html.twig');
+        $orders = $this->entityManager->getRepository(Order::class)->findSuccessOrders($this->getUser());
+
+        return $this->render('account/index_order.html.twig', [
+            'orders' => $orders
+        ]);
+    }
+
+    /**
+     * @Route("/compte/mes-commandes/{reference}", name="account_order_show")
+     */
+    public function show($reference): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByReference($reference);
+
+        if (!$order || $order->getUser() != $this->getUser()) {
+            return $this->redirectToRoute('account_order');
+        }
+
+        return $this->render('account/show_order.html.twig', [
+            'order' => $order
+        ]);
     }
 }
