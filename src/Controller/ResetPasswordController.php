777f710f1bb2bc72e7b407533d1d9076890057a2
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2021-04-09 08:55:27 +0200

    Add ResetPassword entity with controller and template to reset password

---
+++ src/Controller/ResetPasswordController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResetPasswordController extends AbstractController
+{
+    /**
+     * @Route("/mot-de-passe-oublié", name="reset_password")
+     */
+    public function index(): Response
+    {
+        return $this->render('reset_password/index.html.twig');
+    }
+}
